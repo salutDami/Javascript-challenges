15e17f43811f63821a823e1a5811eb997d93f461
Ikuomola Stephen <cstephenid@yahoo.com>
2022-07-15 01:27:25 +0100

    make chnages to javascirpt file

--- logic.js
+++ logic.js
@@ -92,4 +92,27 @@ Given an array of forecasted maximun temperatures, the thermometer display a str
 
 // Solution 
 
+let temperatureData = [17, 21, 23];
+
+temperatureData.forEach((printforecast,i) =>{
+  const result = (`... ${printforecast}°C in ${(i + 1)} days`);
+  console.log(result); 
+}) 
+
+
+// const data1 = [12, 5, -5, 0, 4];
+// const data2 = [17, 21, 23];
+
+
+// const printforecast = function(arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}°C in ${i + 1}  days ...`;
+//   }
+//   console.log('...' + str);
+// };
+
+// printforecast(data2);
+
+
 
